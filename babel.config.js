36f0805a7d7c4e0ac132419c13bf45c635b0cb0b
camisbrussi <camila.sbrussi@universo.univates.br>
2022-03-18 15:42:11 -0300

    feat(babel): configuration

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
   ],
   plugins: [
     [
-      "module-resolver",
+      'module-resolver',
       {
         alias: {
-          "@modules": "./src/modules",
-          "@config": "./src/config",
-          "@shared": "./src/shared",
-          "@errors": "./src/errors",
-          "@utils": "./src/utils"
-        }
-      }
+          '@modules': './src/modules',
+          '@config': './src/config',
+          '@shared': './src/shared',
+          '@errors': './src/errors',
+          '@utils': './src/utils',
+        },
+      },
     ],
-    "babel-plugin-transform-typescript-metadata",
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { "loose": true }]
-  ]
-}
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+  ],
+};
